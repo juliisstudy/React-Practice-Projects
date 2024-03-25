a90fa9de5c3b92480edee00bcec8b0b2827ce508
Juli <julianalicode@gmail.com>
2024-03-25 16:48:22 -0400

    star-rating

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import Accordian from './components/accordion';
 import MultiSelection from './components/accordion/MultiSelection';
 import RandomColor from './components/random-color';
+import StarRating from './components/star-rating';
 
 function App() {
   return (
     <div className="App">
      {/* <Accordian /> */}
      {/* <MultiSelection /> */}
-     <RandomColor/>
+     {/* <RandomColor/> */}
+     {/* <StarRating nuOfStars={10}/> */}
     </div>
   );
 }
---
+++ my-app/src/components/Slideshow/index.jsx
@@ -0,0 +1,1 @@
+//https://picsum.photos/v2/list?page=1&limit%20=5
