bb1d5205a7d0c489051076da8c7f13f4dc3af0e0
Juli <julianalicode@gmail.com>
2024-03-28 14:31:02 -0400

    modal

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,31 +1,33 @@
-import './App.css';
-import Accordian from './components/accordion';
-import MultiSelection from './components/accordion/MultiSelection';
-import RandomColor from './components/random-color';
-import StarRating from './components/star-rating';
+import "./App.css";
+import Accordian from "./components/accordion";
+import MultiSelection from "./components/accordion/MultiSelection";
+import RandomColor from "./components/random-color";
+import StarRating from "./components/star-rating";
 import ImageSlider from "./components/Slideshow";
-import LoadMoreData from './components/load-more-data';
-import TreeView from './components/tree-view';
-import menus from './components/tree-view/data';
-import QRCodeGenerator from './components/qr-code'
-import LightDarkMode from './components/light-dark-mode'
-import ScrollIndicator from './components/scroll-indicator';
-import TabTest from './components/tab/tab-test';
+import LoadMoreData from "./components/load-more-data";
+import TreeView from "./components/tree-view";
+import menus from "./components/tree-view/data";
+import QRCodeGenerator from "./components/qr-code";
+import LightDarkMode from "./components/light-dark-mode";
+import ScrollIndicator from "./components/scroll-indicator";
+import TabTest from "./components/tab/tab-test";
+import ModalTest from "./components/custom-modal-popup/modal-test";
 
 function App() {
   return (
     <div className="App">
-     {/* <Accordian /> */}
-     {/* <MultiSelection /> */}
-     {/* <RandomColor/> */}
-     {/* <StarRating nuOfStars={10}/> */}
-     {/* <ImageSlider url ={"https://picsum.photos/v2/list"} /> */}
-     {/* <LoadMoreData /> */}
-     {/* <TreeView menus={menus}/> */}
-     {/* <QRCodeGenerator/> */}
-     {/* <LightDarkMode /> */}
-     {/* <ScrollIndicator url={'https://dummyjson.com/products?limit=100'} /> */}
-     <TabTest />
+      {/* <Accordian /> */}
+      {/* <MultiSelection /> */}
+      {/* <RandomColor/> */}
+      {/* <StarRating nuOfStars={10}/> */}
+      {/* <ImageSlider url ={"https://picsum.photos/v2/list"} /> */}
+      {/* <LoadMoreData /> */}
+      {/* <TreeView menus={menus}/> */}
+      {/* <QRCodeGenerator/> */}
+      {/* <LightDarkMode /> */}
+      {/* <ScrollIndicator url={'https://dummyjson.com/products?limit=100'} /> */}
+      {/* <TabTest /> */}
+      <ModalTest />
     </div>
   );
 }
---
+++ my-app/src/components/custom-modal-popup/modal-test.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import Modal from "./modal";
+import "./modal.css";
+export default function ModalTest() {
+  const [showModalPopup, setShowModalPopup] = useState(false);
+  function handleToggleModalPopup() {
+    setShowModalPopup(!showModalPopup);
+  }
+  function onClose() {
+    setShowModalPopup(false);
+  }
+  return (
+    <div>
+      <button onClick={handleToggleModalPopup}>open Modal popup</button>
+      {showModalPopup && (
+        <Modal onClose={onClose} body={<div>Customized body</div>} />
+      )}
+    </div>
+  );
+}
