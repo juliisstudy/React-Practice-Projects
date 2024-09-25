051028d2f9f001c9c03f30591739e482e31f1c1e
Juli <julianalicode@gmail.com>
2024-09-25 12:59:09 -0400

    update

---
+++ practices/my-app/app/components/tree-view/index.tsx
@@ -0,0 +1,10 @@
+export default function TreeView({menus=[]}){
+    return (
+        <div className="tree-view-container">
+            {
+                
+            }
+
+        </div>
+    )
+}
